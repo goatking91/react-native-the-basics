d2710a43fe62e8143a5ce92e70cd8d431a6740d9
Selim.Yeom <selim.yeom@lfin.kr>
2019-12-31 14:59:59 +0900

    # Props

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import LotsOfGreetings from './props/LotsOfGreetings';
 
-class HelloWorldApp extends Component {
+class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Hello, world!</Text>
-      </View>
+      <>
+        <LotsOfGreetings />
+      </>
     );
   }
 }
 
-export default HelloWorldApp;
+export default App;
